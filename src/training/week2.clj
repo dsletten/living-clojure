892209912bdda0cd653653e47d8b99c680560a30
David Sletten <david@bosatsu.net>
2021-03-13 11:24:14 -0500

    Halfway through week 3.

--- src/training/week2.clj
+++ src/training/week2.clj
@@ -35,7 +35,7 @@
 ;;;    Day 1 210303
 ;;;
 ;;;    https://www.4clojure.com/problem/26
-;;;
+;;;    Fibonacci
 ;;;    Old notes pg. 126
 (fn [n] (take n (map first (iterate (fn [[a b]] [b (+ a b)]) [1 1]))))
 
@@ -102,3 +102,73 @@
 ;;;    https://www.4clojure.com/problem/90
 ;;;
 (fn [as bs] (set (for [a as b bs] [a b])))
+
+;;;
+;;;    Day 4 210307
+;;;
+;;;
+;;;    https://www.4clojure.com/problem/88
+;;;
+(fn [a b] (clojure.set/union (clojure.set/difference a b) (clojure.set/difference b a)))
+
+;;;
+;;;    https://www.4clojure.com/problem/100
+;;;    https://en.wikipedia.org/wiki/Least_common_multiple
+;;;
+;;;    This problem is ill-defined. It states: "Your function should accept a variable number of positive integers or ratios."
+;;;    1. LCM is only defined for integers, not the entire set of rational numbers.
+;;;    2. LCM is reasonable for negative integers, but ABS should be used.
+;;;       However, Clojure does not behave properly with absolute values of ratios (See 1.):
+;;;       (Math/abs (/ 2 3)) => Execution error (IllegalArgumentException) No matching method abs found taking 1 args
+;;;
+;;;    SBCL:
+;;;    * (lcm 1/3 2/5) => TYPE-ERROR The value 1/3 is not of type INTEGER
+;;;    * (lcm -4 12 -15) => 60
+;;;
+;;;    4clojure answer:
+(fn [& ns] (reduce (fn [a b] (let [gcd (loop [m a n b] (if (zero? n) m (recur n (mod m n))))] (/ (* a b) gcd))) ns))
+;;;    Should be:
+(fn [& ns] (reduce (fn [a b] (let [gcd (loop [m a n b] (if (zero? n) (Math/abs m) (recur n (mod m n))))] (/ (Math/abs (* a b)) gcd))) ns))
+
+;;;
+;;;    Day 5 210308
+;;;
+;;;
+;;;    https://www.4clojure.com/problem/88
+;;;
+(defn pascal [n]
+  (if (zero? n)
+    [1]
+    (let [p (pascal (dec n))]
+      (vec (map + (cons 0 p) (conj p 0)))) ))
+
+(defn pascal [n]
+  (loop [i 0
+         result [1]]
+    (if (== i n)
+      result
+      (recur (inc i) (vec (map + (cons 0 result) (conj result 0)))) )))
+
+(defn pascal [n]
+  (first (take 1 (drop n (iterate #(vec (map + (cons 0 %) (conj % 0))) [1])))) )
+
+(defn pascal [n]
+  (nth (iterate #(vec (map + (cons 0 %) (conj % 0))) [1]) n))
+
+(defn pascal [n]
+  (if (== n 1)
+    [1]
+    (let [p (pascal (dec n))]
+      (vec (map + (cons 0 p) (conj p 0)))) ))
+
+(fn pascal [n]
+  (if (== n 1)
+    [1]
+    (let [p (pascal (dec n))]
+      (vec (map + (cons 0 p) (conj p 0)))) ))
+
+(fn [n] (first (take 1 (drop (dec n) (iterate #(vec (map + (cons 0 %) (conj % 0))) [1])))) )
+(fn [n] (nth (iterate #(vec (map + (cons 0 %) (conj % 0))) [1]) (dec n)))
+
+;;;  !!!!
+;((fn [n] (nth (iterate #(vec (map +' (cons 0 %) (conj % 0))) [1]) (dec n))) 1000)
---
+++ src/training/week3.clj
@@ -0,0 +1,100 @@
+;;;;
+;;;;
+;;;;   Clojure feels like a general-purpose language beamed back
+;;;;     from the near future.
+;;;;   -- Stu Halloway
+;;;;
+;;;;   Name:               week3.clj
+;;;;
+;;;;   Started:            Wed Mar 10 17:52:28 2021
+;;;;   Modifications:
+;;;;
+;;;;   Purpose:
+;;;;
+;;;;
+;;;;
+;;;;   Calling Sequence:
+;;;;
+;;;;
+;;;;   Inputs:
+;;;;
+;;;;   Outputs:
+;;;;
+;;;;   Example:
+;;;;
+;;;;   Notes:
+;;;;
+;;;;
+
+(ns week3
+  (:use clojure.test
+        [clojure.pprint :only (cl-format)])
+  (:import))
+
+;;;
+;;;    Day 1 210310
+;;;    Not thrilled with either of these...
+;;;
+;;;    https://www.4clojure.com/problem/95
+;;;
+(fn tree? [obj]
+  (cond (nil? obj) true
+        (not (coll? obj)) false
+        (not (== (count obj) 3)) false
+        :otherwise (let [[val left right] obj]
+                     (and (tree? left)
+                          (tree? right)))) )
+
+;;;
+;;;    https://www.4clojure.com/problem/96
+;;;
+(fn sym?
+  ([tree] (cond (nil? tree) true
+                (not (coll? tree)) false
+                (not (== (count tree) 3)) false
+                :otherwise (let [[val left right] tree]
+                             (sym? left right))))
+  ([left right]
+   (cond (and (nil? left) (nil? right)) true
+         (not (and (coll? left) (coll? right))) false
+         (not (== (count left) (count right) 3)) false
+         :otherwise (let [[lval lleft lright] left
+                          [rval rleft rright] right]
+                      (and (= lval rval)
+                           (sym? lleft rright)
+                           (sym? lright rleft))))))
+
+;;;
+;;;    Day 2 210311
+;;;
+;;;    https://www.4clojure.com/problem/46
+;;;
+(fn [f] (fn [& args] (apply f (reverse args))))
+
+;;;
+;;;    https://www.4clojure.com/problem/44
+;;;
+(fn rotate [n seq]
+  (cond (pos? n) (let [m (mod n (count seq))]
+                   (concat (drop m seq) (take m seq)))
+        (neg? n) (rotate (+ (count seq) n) seq)
+        :else seq))
+    
+;;;
+;;;    Day 3 210312
+;;;
+;;;    https://www.4clojure.com/problem/43
+;;;    Any better way...???
+;;;
+;(defn segregate [seq n]
+(fn [seq n]
+  (loop [result (take n (repeat []))
+         s seq]
+    (if (empty? s)
+      result
+      (recur (map conj result (take n s)) (drop n s)))) )
+
+;;;
+;;;    https://www.4clojure.com/problem/50
+;;;
+(fn [s] (vals (group-by class s)))
