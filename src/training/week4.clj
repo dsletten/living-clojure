1c6bb58bf96d0d4ea81611169949c2385f1cbd7b
David Sletten <david@bosatsu.net>
2021-03-13 11:30:31 -0500

    Prepping for week 4 activities.

---
+++ src/training/week4.clj
@@ -0,0 +1,33 @@
+;;;;
+;;;;
+;;;;   Clojure feels like a general-purpose language beamed back
+;;;;     from the near future.
+;;;;   -- Stu Halloway
+;;;;
+;;;;   Name:               week4.clj
+;;;;
+;;;;   Started:            Sat Mar 13 11:29:34 2021
+;;;;   Modifications:
+;;;;
+;;;;   Purpose:
+;;;;
+;;;;
+;;;;
+;;;;   Calling Sequence:
+;;;;
+;;;;
+;;;;   Inputs:
+;;;;
+;;;;   Outputs:
+;;;;
+;;;;   Example:
+;;;;
+;;;;   Notes:
+;;;;
+;;;;
+
+(ns week4
+  (:use clojure.test
+        [clojure.pprint :only (cl-format)])
+  (:import))
+
